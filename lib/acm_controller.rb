85e5347186bd70a9d094f041f7412542ee81b196
Joel D'sa <jdsa@vmware.com>
2011-11-15 15:47:30 -0800

    Basic auth for the acm controller

--- lib/acm_controller.rb
+++ lib/acm_controller.rb
@@ -26,15 +26,10 @@ module ACM
 
         @logger.debug("Created ApiController")
 
-        #if perform_auth?(env)
-        #  app = Rack::Auth::Basic.new(api_controller) do |user, password|
-        #    api_controller.authenticate(user, password)
-        #  end
-        #
-        #  app.realm = "Collab Spaces"
-        #else
-          app = api_controller
-        #end
+        app = Rack::Auth::Basic.new(api_controller) do |user, password|
+          api_controller.authenticate(user, password)
+        end
+        app.realm = "ACM"
 
         status, headers, body = app.call(env)
         headers["Date"] = Time.now.rfc822 # As thin doesn't inject date
@@ -42,13 +37,6 @@ module ACM
         [ status, headers, body ]
       end
 
-
-      #TODO: Need to modify this for the UAA
-      def perform_auth?(env)
-        auth_needed   = !PUBLIC_URLS.include?(env["PATH_INFO"])
-        auth_provided = %w(HTTP_AUTHORIZATION X-HTTP_AUTHORIZATION X_HTTP_AUTHORIZATION).detect{ |key| env.has_key?(key) }
-        auth_needed || auth_provided
-      end
     end
 
   end
